d9b5372c25082fc4bbd82c08d211a973219c89d3
Akash Pal <creativeakashpal@gmail.com>
2023-07-13 10:55:36 +0530

    chaged component to use dynamic data

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,17 +1,14 @@
 import "./feed.css";
 import Share from "../share/Share";
 import Post from "../post/Post";
+import { Posts } from "../../dummy-data";
 
 export default function Feed() {
   return (
     <div className="feed">
       <div className="feed-wrapper">
         <Share/> 
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
+        {Posts.map(p => <Post key={p.id} post={p}/>)}
       </div>
     </div>
   )
